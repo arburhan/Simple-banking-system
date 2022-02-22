62353ea16acb0135b37a9d5c059c33a239dc55a9
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-02-22 12:45:52 +0600

    error handle half finish

--- bank-function.js
+++ bank-function.js
@@ -2,26 +2,34 @@
 function choiceDWId(choiceId, isAdd){
     // input amount
     const inputId = document.getElementById(choiceId+'-amount');
-    const inputIdValue = inputId.value;
-    // add amount deposit and withdraw
-    let addDW = document.getElementById(choiceId+'-total');
-    const addDWText = addDW.innerText;
-    const addDWTotal = parseFloat(addDWText) + parseFloat(inputIdValue);
-    addDW.innerText = addDWTotal;
-    // clean input field
-    inputId.value = '';
-    // balance section
-    let balanceId = document.getElementById('balance-total');
-    const balanceIdText = balanceId.innerText;
-    if(isAdd){
-        const withdrawBalance = parseFloat(inputIdValue) + parseFloat(balanceIdText);
-        balanceId.innerText = withdrawBalance;
-    }
-    else{
-        const withdrawBalance = parseFloat(balanceIdText) - parseFloat(inputIdValue);
-        balanceId.innerText = withdrawBalance;
-    }
-    
+//    error handle condition 
+   if(inputId.value > 0){
+        const inputIdValue = inputId.value;
+        // add amount deposit and withdraw
+        let addDW = document.getElementById(choiceId+'-total');
+        const addDWText = addDW.innerText;
+        const addDWTotal = parseFloat(addDWText) + parseFloat(inputIdValue);
+        addDW.innerText = addDWTotal;
+        // clean input field
+        inputId.value = '';
+        // balance section
+        let balanceId = document.getElementById('balance-total');
+        const balanceIdText = balanceId.innerText;
+        if(isAdd){
+            const withdrawBalance = parseFloat(inputIdValue) + parseFloat(balanceIdText);
+            balanceId.innerText = withdrawBalance;
+        }
+        else{
+            const withdrawBalance = parseFloat(balanceIdText) - parseFloat(inputIdValue);
+            balanceId.innerText = withdrawBalance;
+        }
+   }
+   else{
+       // clean input field
+       inputId.value = '';
+       console.log(':)')
+
+   }
 }
 
 // withdraw
