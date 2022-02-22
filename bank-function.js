c783a833f3615852d7328c7fc5e53c35d14d3f37
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-02-22 12:26:12 +0600

    balance section condition finish

--- bank-function.js
+++ bank-function.js
@@ -1,5 +1,5 @@
 // common function
-function choiceDWId(choiceId){
+function choiceDWId(choiceId, isAdd){
     // input amount
     const inputId = document.getElementById(choiceId+'-amount');
     const inputIdValue = inputId.value;
@@ -10,13 +10,25 @@ function choiceDWId(choiceId){
     addDW.innerText = addDWTotal;
     // clean input field
     inputId.value = '';
+    // balance section
+    let balanceId = document.getElementById('balance-total');
+    const balanceIdText = balanceId.innerText;
+    if(isAdd){
+        const withdrawBalance = parseFloat(inputIdValue) + parseFloat(balanceIdText);
+        balanceId.innerText = withdrawBalance;
+    }
+    else{
+        const withdrawBalance = parseFloat(balanceIdText) - parseFloat(inputIdValue);
+        balanceId.innerText = withdrawBalance;
+    }
+    
 }
 
 // withdraw
 document.getElementById('deposit-button').addEventListener('click', function(){
-    choiceDWId('deposit');
+    choiceDWId('deposit', true);
 })
 // deposit
 document.getElementById('withdraw-button').addEventListener('click', function(){
-    choiceDWId('withdraw');
+    choiceDWId('withdraw', false);
 })
